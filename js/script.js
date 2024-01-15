aeee97e51defd41858905e8dc07cf19022a89acc
talhajutt-01 <talhaj928@gmail.com>
2024-01-16 00:02:34 +0500

    first

---
+++ js/script.js
@@ -0,0 +1,433 @@
+const title = document.getElementById('title');
+
+const h0 = document.getElementById('h0');
+const h1 = document.getElementById('h1');
+const h2 = document.getElementById('h2');
+const h3 = document.getElementById('h3');
+const h4 = document.getElementById('h4');
+const h5 = document.getElementById('h5');
+
+const nav0 = document.getElementById('nav0');
+const nav1 = document.getElementById('nav1');
+const nav2 = document.getElementById('nav2');
+const nav3 = document.getElementById('nav3');
+const nav4 = document.getElementById('nav4');
+const nav5 = document.getElementById('nav5');
+
+const p0 = document.getElementById('p0');
+const p1 = document.getElementById('p1');
+const p2 = document.getElementById('p2');
+const p3 = document.getElementById('p3');
+const p4 = document.getElementById('p4');
+
+const ca0 = document.getElementById('ca0');
+const ca1 = document.getElementById('ca1');
+const ca2 = document.getElementById('ca2');
+const ca3 = document.getElementById('ca3');
+
+
+
+const a0 = document.getElementById('a0');
+const a1 = document.getElementById('a1');
+const a2 = document.getElementById('a2');
+const a4 = document.getElementById('a4');
+const a5 = document.getElementById('a5');
+const a6 = document.getElementById('a6');
+const a7 = document.getElementById('a7');
+const a8 = document.getElementById('a8');
+const a9 = document.getElementById('a9');
+
+const s0 = document.getElementById('s0');
+const s1 = document.getElementById('s1');
+const s2 = document.getElementById('s2');
+const s3 = document.getElementById('s3');
+const s4 = document.getElementById('s4');
+const s5 = document.getElementById('s5');
+const s6 = document.getElementById('s6');
+const s7 = document.getElementById('s7');
+const s8 = document.getElementById('s8');
+const s9 = document.getElementById('s9');
+const s10 = document.getElementById('s10');
+const s11 = document.getElementById('s11');
+const s12 = document.getElementById('s12');
+const s13 = document.getElementById('s13');
+const s14 = document.getElementById('s14');
+const s15 = document.getElementById('s15');
+const s16 = document.getElementById('s16');
+const s17 = document.getElementById('s17');
+
+const ap0 = document.getElementById('ap0');
+const ap1 = document.getElementById('ap1');
+const ap2 = document.getElementById('ap2');
+
+const d0 = document.getElementById('d0');
+const d1 = document.getElementById('d1');
+const d2 = document.getElementById('d2');
+const d3 = document.getElementById('d3');
+const d4 = document.getElementById('d4');
+const d5 = document.getElementById('d5');
+const d6 = document.getElementById('d6');
+const d7 = document.getElementById('d7');
+const d8 = document.getElementById('d8');
+const d9 = document.getElementById('d9');
+const d10 = document.getElementById('d10');
+const d11 = document.getElementById('d11');
+const d12 = document.getElementById('d12');
+const d13 = document.getElementById('d13');
+const d14 = document.getElementById('d14');
+const d15 = document.getElementById('d15');
+
+const t0 = document.getElementById('t0');
+const t1 = document.getElementById('t1');
+const t2 = document.getElementById('t2');
+const t3 = document.getElementById('t3');
+const t4 = document.getElementById('t4');
+const t5 = document.getElementById('t5');
+const t6 = document.getElementById('t6');
+const t7 = document.getElementById('t7');
+const t8 = document.getElementById('t8');
+
+const te0 = document.getElementById('te0');
+const te1 = document.getElementById('te1');
+const te2 = document.getElementById('te2');
+const te3 = document.getElementById('te3');
+const te4 = document.getElementById('te4');
+const te5 = document.getElementById('te5');
+const te6 = document.getElementById('te6');
+
+const b0 = document.getElementById('b0');
+const b1 = document.getElementById('b1');
+const b2 = document.getElementById('b2');
+const b3 = document.getElementById('b3');
+const b4 = document.getElementById('b4');
+const b5 = document.getElementById('b5');
+const b6 = document.getElementById('b6');
+const b7 = document.getElementById('b7');
+const b8 = document.getElementById('b8');
+const b9 = document.getElementById('b9');
+const b10 = document.getElementById('b10');
+const b11 = document.getElementById('b11');
+const b12 = document.getElementById('b12');
+
+
+const f0 = document.getElementById('f0');
+const f1 = document.getElementById('f1');
+const f2 = document.getElementById('f2');
+const f3 = document.getElementById('f3');
+const f4 = document.getElementById('f4');
+const f5 = document.getElementById('f5');
+const f6 = document.getElementById('f6');
+const f7 = document.getElementById('f7');
+const f8 = document.getElementById('f8');
+const f9 = document.getElementById('f9');
+const f10 = document.getElementById('f10');
+
+const u0 = document.getElementById('u0');
+const u1 = document.getElementById('u1');
+const u2 = document.getElementById('u2');
+const u3 = document.getElementById('u3');
+const u4 = document.getElementById('u4');
+const u5 = document.getElementById('u5');
+const u6 = document.getElementById('u6');
+const u7 = document.getElementById('u7');
+const u8 = document.getElementById('u8');
+const u9 = document.getElementById('u9');
+const u10 = document.getElementById('u10');
+const u11 = document.getElementById('u11');
+const u12 = document.getElementById('u12');
+const u13 = document.getElementById('u13');
+const u14 = document.getElementById('u14');
+const u15 = document.getElementById('u15');
+const u16 = document.getElementById('u16');
+const u17 = document.getElementById('u17');
+const u18 = document.getElementById('u18');
+const u19 = document.getElementById('u19');
+const u20 = document.getElementById('u20');
+const u21 = document.getElementById('u21');
+const u22 = document.getElementById('u22');
+const u23 = document.getElementById('u23');
+const u24 = document.getElementById('u24');
+const u25 = document.getElementById('u25');
+
+
+
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const img5 = document.getElementById('img5');
+const img6 = document.getElementById('img6');
+const img7 = document.getElementById('img7');
+const img8 = document.getElementById('img8');
+const img9 = document.getElementById('img9');
+const img10 = document.getElementById('img10');
+const img11 = document.getElementById('img11');
+const img12 = document.getElementById('img12');
+const img13 = document.getElementById('img13');
+const img14 = document.getElementById('img14');
+const img15 = document.getElementById('img15');
+const img16 = document.getElementById('img16');
+const img17 = document.getElementById('img17');
+const img18 = document.getElementById('img18');
+const img19 = document.getElementById('img19');
+const img20 = document.getElementById('img20');
+const img21 = document.getElementById('img21');
+const img22 = document.getElementById('img22');
+const img23 = document.getElementById('img23');
+const img24 = document.getElementById('img24');
+const img25 = document.getElementById('img25');
+const img26 = document.getElementById('img26');
+const img27 = document.getElementById('img27');
+const img28 = document.getElementById('img28');
+const img29 = document.getElementById('img28');
+const img30 = document.getElementById('img30');
+const img31 = document.getElementById('img31');
+const img32 = document.getElementById('img32');
+
+
+// Function to update content for a specific element
+function updateContent(element, newValue) {
+    // Update content for the element
+    element.textContent = newValue;
+  }
+  
+  // Function to update content for an <a> tag
+  function updateLinkContent(linkElement, newValue) {
+    // Update content for the <a> tag
+    linkElement.innerText = newValue;
+  }
+
+  function updateImageSrc(imageElement, newSrc) {
+    // Update the src attribute of the image element
+    imageElement.src = newSrc;
+}
+
+  
+  // Fetch JSON data from the text file
+  function updatePageContent(pageName) {
+    console.log('Updating content for page:', pageName);
+    fetch('t.txt')
+        .then(response => response.json())
+        .then(jsonData => {
+            // Update content based on the current page
+            updateContent(title, jsonData.title.title);
+
+
+            updateLinkContent(h0, jsonData.topbar.h0);
+            updateLinkContent(h1, jsonData.topbar.h1);
+            updateLinkContent(h2, jsonData.topbar.h2);
+            updateLinkContent(h3, jsonData.topbar.h3);
+            updateLinkContent(h4, jsonData.topbar.h4);
+            updateLinkContent(h5, jsonData.topbar.h5);
+
+            updateLinkContent(nav0, jsonData.nav.nav0);
+            updateLinkContent(nav1, jsonData.nav.nav1);
+            updateLinkContent(nav2, jsonData.nav.nav2);
+            updateLinkContent(nav3, jsonData.nav.nav3);
+            updateLinkContent(nav4, jsonData.nav.nav4);
+            updateLinkContent(nav5, jsonData.nav.nav5);
+
+            updateContent(p0, jsonData.page.p0);
+            updateContent(p1, jsonData.page.p1);
+            updateContent(p2, jsonData.page.p2);
+            updateContent(p3, jsonData.page.p3);
+            updateContent(p4, jsonData.page.p4);
+
+            updateContent(ca0, jsonData.carousel.ca0);
+            updateContent(ca1, jsonData.carousel.ca1);
+            updateContent(ca2, jsonData.carousel.ca2);
+            updateContent(ca3, jsonData.carousel.ca3);
+        
+            updateContent(a0, jsonData.about.a0);
+            updateContent(a1, jsonData.about.a1);
+            updateContent(a2, jsonData.about.a2);
+            updateContent(a3, jsonData.about.a3);
+            updateContent(a4, jsonData.about.a4);
+            updateContent(a5, jsonData.about.a5);
+            updateContent(a6, jsonData.about.a6);
+            updateContent(a7, jsonData.about.a7);
+            updateContent(a8, jsonData.about.a8);
+            updateContent(a9, jsonData.about.a9);
+
+
+            updateContent(s0, jsonData.service.s0);
+            updateContent(s1, jsonData.service.s1);
+            updateContent(s2, jsonData.service.s2);
+            updateContent(s4, jsonData.service.s4);
+            updateContent(s5, jsonData.service.s5);
+            updateContent(s6, jsonData.service.s6);
+            updateContent(s7, jsonData.service.s7);
+            updateContent(s8, jsonData.service.s8);
+            updateContent(s9, jsonData.service.s9);
+            updateContent(s10, jsonData.service.s10);
+            updateContent(s11, jsonData.service.s11);
+            updateContent(s12, jsonData.service.s12);
+            updateContent(s13, jsonData.service.s13);
+            updateContent(s14, jsonData.service.s14);
+            updateContent(s15, jsonData.service.s15);
+            updateContent(s16, jsonData.service.s16);
+            updateContent(s17, jsonData.service.s17);
+
+            updateContent(ap0, jsonData.appointment.ap0);
+            updateContent(ap1, jsonData.appointment.ap1);
+            updateContent(ap2, jsonData.appointment.ap2);
+            
+       
+            updateContent(d0, jsonData.portfolio.d0);
+            updateContent(d1, jsonData.portfolio.d1);
+            updateContent(d2, jsonData.portfolio.d2);
+            updateContent(d3, jsonData.portfolio.d3);
+            updateContent(d4, jsonData.portfolio.d4);
+            updateContent(d5, jsonData.portfolio.d5);
+            updateContent(d6, jsonData.portfolio.d6);
+            updateContent(d7, jsonData.portfolio.d7);
+            updateContent(d8, jsonData.portfolio.d8);
+            updateContent(d9, jsonData.portfolio.d9);
+            updateContent(d10, jsonData.portfolio.d10);
+            updateContent(d11, jsonData.portfolio.d11);
+            updateContent(d12, jsonData.portfolio.d12);
+            updateContent(d13, jsonData.portfolio.d13);
+            updateContent(d14, jsonData.portfolio.d14);
+            updateContent(d15, jsonData.portfolio.d15);
+
+            updateContent(t0, jsonData.team.t0);
+            updateContent(t1, jsonData.team.t1);
+            updateContent(t2, jsonData.team.t2);
+            updateContent(t3, jsonData.team.t3);
+            updateContent(t4, jsonData.team.t4);
+            updateContent(t5, jsonData.team.t5);
+            updateContent(t6, jsonData.team.t6);
+            updateContent(t7, jsonData.team.t7);
+            updateContent(t8, jsonData.team.t8);
+
+            updateContent(te0, jsonData.testimonial.te0);
+            updateContent(te1, jsonData.testimonial.te1);
+            updateContent(te2, jsonData.testimonial.te2);
+            updateContent(te3, jsonData.testimonial.te3);
+            updateContent(te4, jsonData.testimonial.te4);
+            updateContent(te5, jsonData.testimonial.te5);
+            updateContent(te6, jsonData.testimonial.te6);
+          
+        
+            updateContent(b0, jsonData.blog.b0);
+            updateContent(b1, jsonData.blog.b1);
+            updateContent(b2, jsonData.blog.b2);
+            updateContent(b3, jsonData.blog.b3);
+            updateContent(b4, jsonData.blog.b4);
+            updateContent(b5, jsonData.blog.b5);
+            updateContent(b6, jsonData.blog.b6);
+            updateContent(b7, jsonData.blog.b7);
+            updateContent(b8, jsonData.blog.b8);
+            updateContent(b9, jsonData.blog.b9);
+            updateContent(b10, jsonData.blog.b10);   
+            updateContent(b11, jsonData.blog.b11);
+            updateContent(b12, jsonData.blog.b12);
+        
+    
+            updateContent(f0, jsonData.footer.f0);
+            updateContent(f1, jsonData.footer.f1);
+            updateContent(f2, jsonData.footer.f2);
+            updateContent(f3, jsonData.footer.f3);
+            updateContent(f4, jsonData.footer.f4);
+            updateContent(f5, jsonData.footer.f5);
+            updateContent(f6, jsonData.footer.f6);
+            updateContent(f7, jsonData.footer.f7);
+            updateContent(f8, jsonData.footer.f8);
+            updateContent(f9, jsonData.footer.f9);
+            updateContent(f10, jsonData.footer.f10); 
+    
+         
+            updateImageSrc(img1, jsonData.images.img1);
+            updateImageSrc(img2, jsonData.images.img2);
+            updateImageSrc(img3, jsonData.images.img3);
+            updateImageSrc(img4, jsonData.images.img4);
+            updateImageSrc(img5, jsonData.images.img5);
+            updateImageSrc(img6, jsonData.images.img6);
+            updateImageSrc(img7, jsonData.images.img7);
+            updateImageSrc(img8, jsonData.images.img8);
+            updateImageSrc(img9, jsonData.images.img9);
+            updateImageSrc(img10, jsonData.images.img10);
+            updateImageSrc(img11, jsonData.images.img11);
+            updateImageSrc(img12, jsonData.images.img12);
+            updateImageSrc(img13, jsonData.images.img13);
+            updateImageSrc(img14, jsonData.images.img14);
+            updateImageSrc(img15, jsonData.images.img15);
+            updateImageSrc(img16, jsonData.images.img16);
+            updateImageSrc(img17, jsonData.images.img17);
+            updateImageSrc(img18, jsonData.images.img18);
+            updateImageSrc(img19, jsonData.images.img19);
+            updateImageSrc(img20, jsonData.images.img20);
+            updateImageSrc(img21, jsonData.images.img21);
+            updateImageSrc(img22, jsonData.images.img22);
+            updateImageSrc(img23, jsonData.images.img23);
+            updateImageSrc(img24, jsonData.images.img24);
+            updateImageSrc(img25, jsonData.images.img25);
+            updateImageSrc(img26, jsonData.images.img26);
+            updateImageSrc(img27, jsonData.images.img27);
+            updateImageSrc(img28, jsonData.images.img28);
+            updateImageSrc(img29, jsonData.images.img29);
+            updateImageSrc(img30, jsonData.images.img30);
+            updateImageSrc(img31, jsonData.images.img31);
+            updateImageSrc(img32, jsonData.images.img32);
+           
+           
+            // Extract the filename from the path
+            const fileName = pageName.split('/').pop();
+
+            if (fileName === 'index.html') {
+               
+            } else if (fileName === 'about.html') {
+
+            } else if (fileName === 'detail.html') {
+            updateContent(u1, jsonData.blogdetail.u1);
+            updateContent(u2, jsonData.blogdetail.u2);
+            updateContent(u3, jsonData.blogdetail.u3);
+            updateContent(u4, jsonData.blogdetail.u4);
+            updateContent(u5, jsonData.blogdetail.u5);
+            updateContent(u6, jsonData.blogdetail.u6);
+            updateContent(u7, jsonData.blogdetail.u7);
+            updateContent(u8, jsonData.blogdetail.u8);
+            updateContent(u9, jsonData.blogdetail.u9);
+            updateImageSrc(u10, jsonData.blogdetail.u10);
+            updateImageSrc(u11, jsonData.blogdetail.u11);
+            updateImageSrc(u12, jsonData.blogdetail.u12);
+            updateImageSrc(u13, jsonData.blogdetail.u13);
+            updateImageSrc(u14, jsonData.blogdetail.u14);
+            updateImageSrc(u15, jsonData.blogdetail.u15);
+            updateImageSrc(u16, jsonData.blogdetail.u16);
+            updateImageSrc(u17, jsonData.blogdetail.u17);
+            updateImageSrc(u18, jsonData.blogdetail.u18);
+            updateImageSrc(u19, jsonData.blogdetail.u19);
+            updateImageSrc(u20, jsonData.blogdetail.u20);
+            updateImageSrc(u21, jsonData.blogdetail.u21);
+            updateImageSrc(u22, jsonData.blogdetail.u22);
+            updateImageSrc(u23, jsonData.blogdetail.u23);
+            updateImageSrc(u24, jsonData.blogdetail.u24);
+            updateImageSrc(u25, jsonData.blogdetail.u25);
+
+            } else if (fileName === 'testimonial.html') {
+
+            } else if (fileName === 'contact.html') {
+
+            } else if (fileName === 'team.html') {
+
+            }else if (fileName === 'project.html') {
+
+            }else if (fileName === 'blog.html') {
+
+            }else if (fileName === 'service.html') {
+
+            }
+        })
+        .catch(error => console.error('Error fetching or parsing JSON:', error));
+}
+
+// Call the function to update content when the page loads
+window.addEventListener('load', function () {
+    console.log('page loaded');
+    // Get the current page URL
+    const currentPage = window.location.pathname;
+
+    // Update content based on the current page
+    updatePageContent(currentPage);
+});
